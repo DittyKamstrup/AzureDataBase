f8777753cd8cd99dd4d916f64314199fbe46aaea
Ditte <Ditte@DESKTOP-QORFADN>
2017-09-26 23:21:34 +0200

    Program opdateret, udangspunkt i eksempel fra tavleundervisning

--- AzureDataBase/AzureDataBase/Program.cs
+++ AzureDataBase/AzureDataBase/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,31 @@ namespace AzureDataBase
 
             string con = "Data Source = dittessqlserver.database.windows.net;Initial Catalog = SchoolDB;Persist Security Info = False;User ID = ditteak; Password = Ditte1234;MultipleActiveResultSets = False;Encrypt = True;TrustServerCertificate = False;Connection Timeout = 30";
 
+
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(SelectAllStudents, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            int ID = reader.GetInt32(0);
+                            string Name = reader.GetString(1);
+                            DateTime DateOfBirth = reader.GetDateTime(2);
+                            string Subject = reader.GetString(3);
+
+                            Console.WriteLine(ID + " " + Name + " " + DateOfBirth + " " + Subject);
+                            
+                        }
+
+                    }
+                }
+
+            }
+            Console.ReadLine();
         }
     }
 }
