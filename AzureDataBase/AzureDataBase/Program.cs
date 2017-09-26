3508afb091fccd2bba6c6f7f30f16bf9b8a81b94
Ditte <Ditte@DESKTOP-QORFADN>
2017-09-26 22:55:57 +0200

    Påbegyndelse af "Azure relational database" opgave

---
+++ AzureDataBase/AzureDataBase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SelectAllStudents = "select * from Students";
+
+            string con = "Data Source = dittessqlserver.database.windows.net;Initial Catalog = SchoolDB;Persist Security Info = False;User ID = ditteak; Password = Ditte1234;MultipleActiveResultSets = False;Encrypt = True;TrustServerCertificate = False;Connection Timeout = 30";
+
+        }
+    }
+}
