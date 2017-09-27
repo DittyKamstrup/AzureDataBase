4b00165cbbd94244c7a01b6bef8655d3c24bec32
Ditte <Ditte@DESKTOP-QORFADN>
2017-09-27 13:19:37 +0200

    Begyndende udgangspunkt i eksempel A fra anders

--- AzureDataBase/AzureDataBase/Program.cs
+++ AzureDataBase/AzureDataBase/Program.cs
@@ -4,42 +4,82 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace AzureDataBase
 {
     class Program
     {
+        private const string ConnectionString = "Data Source = dittessqlserver.database.windows.net;Initial Catalog = SchoolDB;Persist Security Info = False;User ID = ditteak; Password = Ditte1234;MultipleActiveResultSets = False;Encrypt = True;TrustServerCertificate = False;Connection Timeout = 30";
+        private const string SelectAllStudents = "select * from Students";
+        private const string InsertStudent = "insert into student (Name, Subject) values (@Name, @Subject)";
+
         static void Main(string[] args)
         {
-            string SelectAllStudents = "select * from Students";
+            string conString = ConnectionString;
 
-            string con = "Data Source = dittessqlserver.database.windows.net;Initial Catalog = SchoolDB;Persist Security Info = False;User ID = ditteak; Password = Ditte1234;MultipleActiveResultSets = False;Encrypt = True;TrustServerCertificate = False;Connection Timeout = 30";
-
-
-            using (SqlConnection connection = new SqlConnection(con))
+            using (SqlConnection databaseConnection = new SqlConnection(conString))
             {
-                connection.Open();
-                using (SqlCommand selectCommand = new SqlCommand(SelectAllStudents, connection))
-                {
-                    using (SqlDataReader reader = selectCommand.ExecuteReader())
-                    {
-
-                        while (reader.Read())
-                        {
-                            int ID = reader.GetInt32(0);
-                            string Name = reader.GetString(1);
-                            DateTime DateOfBirth = reader.GetDateTime(2);
-                            string Subject = reader.GetString(3);
-
-                            Console.WriteLine(ID + " " + Name + " " + DateOfBirth + " " + Subject);
-                            
-                        }
-
-                    }
-                }
-
+                databaseConnection.Open();
+                //IList<Students> allStudents = GetAllStudents(databaseConnection);
+                //Console.WriteLine(string.Join("\n", allStudents));
+                //InsertStudents(databaseConnection);
+                //allStudents = GetAllStudents(databaseConnection);        Skal først lave metoderne
+                //Console.WriteLine(string.Join("\n", allStudents)); 
             }
-            Console.ReadLine();
+
+            //private static string GetConnectionStringFromAppConfig()
+            //{
+
+
+            //    ConnectionStringSettingsCollection connectionStringSettingsCollection = ConfigurationManager.ConnectionStrings;
+            //    ConnectionStringSettings connStringSettings = connectionStringSettingsCollection["anbo1databaseAzure"];
+            //    string connString = connStringSettings.ConnectionString;
+            //    return connString;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // Fra eksemplet fra tavlen
+
+            //        using (SqlConnection connection = new SqlConnection(con))
+            //        {
+            //            connection.Open();
+            //            using (SqlCommand selectCommand = new SqlCommand(SelectAllStudents, connection))
+            //            {
+            //                using (SqlDataReader reader = selectCommand.ExecuteReader())
+            //                {
+
+            //                    while (reader.Read())
+            //                    {
+            //                        int ID = reader.GetInt32(0);
+            //string Name = reader.GetString(1);
+            //DateTime DateOfBirth = reader.GetDateTime(2);
+            //string Subject = reader.GetString(3);
+
+            //Console.WriteLine(ID + " " + Name + " " + DateOfBirth + " " + Subject);
+
+            //                    }
+
+            //                }
+            //            }
+
+            //        }
+            //        Console.ReadLine();
+            // }
         }
     }
 }
---
+++ AzureDataBase/AzureDataBase/Students.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDataBase
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Subject { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " " + Name + " " + DateOfBirth + " " + Subject;
+        }
+
+
+    }
+}
